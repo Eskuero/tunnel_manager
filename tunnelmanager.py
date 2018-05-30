18c0eab3fb8b5af14df710bda77b888a9ff0bc63
Óliver García Albertos <3skuero@gmail.com>
2018-05-30 14:40:42 +0100

    Initial file importing and tunnel setup

---
+++ tunnelmanager.py
@@ -0,0 +1,19 @@
+import os
+
+print("Helper script to expose local services on a public server")
+
+# Open the file containing the parameters
+config = open("config", "r")
+vars = {}
+
+# Loop through every line to store the variables on a list
+for line in config:
+    line = line.split()
+    vars[line[0]] = line[1]
+
+# Use the recovered values to build the tunnel circuit, the login credentials and the final command
+circuit = vars['REMOTE_PORT'] + ":" + vars['DESTINY_HOST'] + ":" + vars['DESTINY_PORT']
+login = vars['REMOTE_USER'] + "@" + vars['REMOTE_HOST']
+command = "ssh -fNT -R " + circuit + " " + login
+# Setup the SSH tunnel
+os.system(command)
