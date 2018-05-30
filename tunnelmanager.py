ccba84c6886639d2e695403a91a2fbf4a86d4d7e
Óliver García Albertos <3skuero@gmail.com>
2018-05-30 19:15:58 +0100

    Create and enable a SystemD unit

--- tunnelmanager.py
+++ tunnelmanager.py
@@ -27,6 +27,20 @@ for line in config:
 # Use the recovered values to build the tunnel circuit, the login credentials and the final command
 circuit = vars['REMOTE_PORT'] + ":" + vars['DESTINY_HOST'] + ":" + vars['DESTINY_PORT']
 login = vars['REMOTE_USER'] + "@" + vars['REMOTE_HOST']
-command = "ssh -fNT -R " + circuit + " " + login
-# Setup the SSH tunnel
-os.system(command)
+command = "/usr/bin/ssh -NT -R " + circuit + " " + login
+
+# We make sure that the directory for systemd units already exists
+userdir = os.environ["HOME"] + "/.config/systemd/user/"
+os.system("mkdir -p " + userdir + "default.target.wants")
+# We compose a new .unit file for the ssh tunnel
+temp = open("unit.tmp", "w")
+temp.write("[Unit]\n")
+temp.write("Description=Tunnel forwarder\n")
+temp.write("[Service]\n")
+temp.write("ExecStart=" + command + "\n")
+temp.write("[Install]\n")
+temp.write("WantedBy=default.target\n")
+temp.close()
+# We install to the final directory and enable the unit on boot
+os.rename("unit.tmp", userdir + "forward_tunnel.service")
+os.system("systemctl --user enable forward_tunnel.service")
