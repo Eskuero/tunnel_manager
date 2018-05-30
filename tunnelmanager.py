00df9539033ec15bcd9d3b5e9156a368c06b655a
Óliver García Albertos <3skuero@gmail.com>
2018-05-30 15:24:13 +0100

    Allow receiving config file as argument

--- tunnelmanager.py
+++ tunnelmanager.py
@@ -1,9 +1,17 @@
 import os
+import sys
 
 print("Helper script to expose local services on a public server")
 
+# If we received an argument we assume it's the configuration file
+try:
+    file = sys.argv[1]
+# If it was empty we prompt the user to specify one
+except IndexError:
+    file = input("No configuration file was provided, please specify one: ")
+
 # Open the file containing the parameters
-config = open("config", "r")
+config = open(file, "r")
 vars = {}
 
 # Loop through every line to store the variables on a list
