ad9c7d254eeb34fe744adb3083ade06dd8e8e34a
Óliver García Albertos <3skuero@gmail.com>
2018-05-30 19:39:32 +0100

    Use standard libraries whenever possible

--- tunnelmanager.py
+++ tunnelmanager.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 print("Helper script to expose local services on a public server")
 
@@ -27,11 +28,11 @@ for line in config:
 # Use the recovered values to build the tunnel circuit, the login credentials and the final command
 circuit = vars['REMOTE_PORT'] + ":" + vars['DESTINY_HOST'] + ":" + vars['DESTINY_PORT']
 login = vars['REMOTE_USER'] + "@" + vars['REMOTE_HOST']
-command = "/usr/bin/ssh -NT -R " + circuit + " " + login
+command = shutil.which("ssh") + " -NT -R " + circuit + " " + login
 
 # We make sure that the directory for systemd units already exists
 userdir = os.environ["HOME"] + "/.config/systemd/user/"
-os.system("mkdir -p " + userdir + "default.target.wants")
+os.makedirs(userdir + "default.target.wants", exist_ok=True)
 # We compose a new .unit file for the ssh tunnel
 temp = open("unit.tmp", "w")
 temp.write("[Unit]\n")
