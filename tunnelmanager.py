a4de7274ae6fe930da3400c4ce14877207e3ed8b
Óliver García Albertos <3skuero@gmail.com>
2018-05-31 18:02:06 +0100

    Check against missing parameters

--- tunnelmanager.py
+++ tunnelmanager.py
@@ -6,24 +6,43 @@ print("Helper script to expose local services on a public server")
 
 # If we received an argument we assume it's the configuration file
 try:
-    file = sys.argv[1]
+	file = sys.argv[1]
 # If it was empty we prompt the user to specify one
 except IndexError:
-    file = input("No configuration file was provided, please specify one: ")
+	file = input("No configuration file was provided, please specify one: ")
 
 # Open the file containing the parameters
 try:
-    config = open(file, "r")
+	config = open(file, "r")
 except FileNotFoundError:
-    print("The file " + file + " was not found.")
-    sys.exit(1)
-vars = {}
+	print("The file " + file + " was not found.")
+	sys.exit(1)
+# Default parameter values
+vars = {
+	"REMOTE_PORT": "",
+	"REMOTE_HOST": "",
+	"REMOTE_USER": os.environ['USER'],
+	"DESTINY_PORT": "",
+	"DESTINY_HOST": "localhost",
+	"TUNNEL_NAME": "",
+	"LOAD_ONBOOT": "no"
+}
 
 # Loop through every line to store the variables on a list
 for line in config:
-    if line[0] != "#":
-        line = line.split()
-        vars[line[0]] = line[1]
+	if line[0] != "#":
+		line = line.split()
+		vars[line[0]] = line[1]
+
+# We loop through every parameter and report empty ones
+error = "false"
+for (key, value) in vars.items():
+	if value == "":
+		print("The " + key + " parameter is not set")
+		error = "true"
+if error == "true":
+	print("Fix missing parameters and try again")
+	sys.exit(1)
 
 # Use the recovered values to build the tunnel circuit, the login credentials and the final command
 circuit = vars['REMOTE_PORT'] + ":" + vars['DESTINY_HOST'] + ":" + vars['DESTINY_PORT']
